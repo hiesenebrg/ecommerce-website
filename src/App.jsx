3c61112e71ef5673945eec225a75c7c055112ff8
hiesenebrg <adarshsingh8008@gmail.com>
2022-11-11 16:35:09 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Route,Routes,Link} from "react-router-dom";
+import { Route, Routes, Link,  } from "react-router-dom";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
@@ -8,26 +8,24 @@ import Cart from "./pages/Cart";
 import Navbar from "./components/Navbar";
 // so people using the keyboard for navigation or screen readers will still be able to use this app.import React from "react";
 
-
-
-
 const App = () => {
-  
-  <div className="App">
-      
-  <Routes>
-
-  <Route exact path="/" element={<Home />} />
-  <Route exact path="/cart" element={<Cart />} />
-  <Route exact path="/register" element={<Register />} />
-  <Route exact path="/login" element={<Login />} />
-  <Route exact path="/product" element={<Product />} />
-  <Route exact path="/productlist" element={<ProductList />} />
-
-  </Routes>
-  
-  </div>
-  
+  return (
+    <div className="App">
+      <Routes>
+       
+        <Route path="/" element={<Aisehi />}>
+          <Route index element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productlist" element={<ProductList />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
+
+
 
 export default App;
